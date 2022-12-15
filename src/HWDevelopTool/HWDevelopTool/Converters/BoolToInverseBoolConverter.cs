7d920a12213150462a6ae8501e0f5889083cb652
zrq-github <46354863+zrq-github@users.noreply.github.com>
2022-12-15 20:26:22 +0800

    1

---
+++ src/HWDevelopTool/HWDevelopTool/Converters/BoolToInverseBoolConverter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using System.Windows;
+using System.Windows.Data;
+
+namespace HWDevelopTool.Converters;
+
+[ValueConversion(typeof(bool), typeof(bool))]
+class BoolToInverseBoolConverter
+    : IValueConverter
+{
+    public static readonly BoolToInverseBoolConverter Instance = new();
+
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        if (!(value is bool boolValue))
+            return DependencyProperty.UnsetValue;
+
+        return !boolValue;
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        return Convert(value, targetType, parameter, culture);
+    }
+}
---
+++ src/HWDevelopTool/HWDevelopTool/ViewModels/ApplicationViewModel.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using HWDevelopTool.Framework;
+
+namespace HWDevelopTool.ViewModels
+{
+    class ApplicationViewModel
+        : ViewModel
+    {
+
+        private bool _isReadOnly;
+
+        public bool IsReadOnly
+        {
+            get => _isReadOnly;
+            set => SetProperty(ref _isReadOnly, value);
+        }
+
+        private bool _isDeveloperMode;
+
+        private bool _isTitleBarVisible = false;
+
+        public bool IsTitleBarVisible
+        {
+            get => _isTitleBarVisible;
+            set => SetProperty(ref _isTitleBarVisible, value);
+        }
+
+        public ApplicationViewModel()
+        {
+
+        }
+    }
+}
