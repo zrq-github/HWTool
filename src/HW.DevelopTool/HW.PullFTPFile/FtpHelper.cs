5bffb314a0199cd7d22b01c66c270ff0460c0e17
zrq-github <46354863+zrq-github@users.noreply.github.com>
2022-12-17 17:40:40 +0800

    拉包界面

---
+++ src/HW.DevelopTool/HW.PullFTPFile/FtpHelper.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW.PullFTPFile
+{
+    internal class FtpHelper
+    {
+        /// <summary>
+        /// 从ftp服务器上获得文件列表
+        /// </summary>
+        /// <param name="RequedstPath">服务器下的相对路径</param>
+        /// <returns></returns>
+        public static string GetFile(string RequedstPath)
+        {
+            string strs = "";
+            try
+            {
+                string uri = RequedstPath;   //目标路径 path为服务器地址
+                FtpWebRequest reqFTP = (FtpWebRequest)FtpWebRequest.Create(new Uri(uri));
+                // ftp用户名和密码
+                reqFTP.Credentials = new NetworkCredential();
+                reqFTP.Method = WebRequestMethods.Ftp.ListDirectoryDetails;
+                WebResponse response = reqFTP.GetResponse();
+                StreamReader reader = new StreamReader(response.GetResponseStream());//中文文件名
+
+                string line = reader.ReadLine();
+                long tempdata = 0;
+                while (line != null)
+                {
+                    if (!line.Contains("<DIR>"))
+                    {
+                        long data = long.Parse(line.Substring(line.LastIndexOf('_') + 1, 12));
+                        string msg = line.Substring(48).Trim();
+                        if (data > tempdata)
+                        {
+                            tempdata = data;
+                            strs = msg;
+                        }
+                    }
+                    line = reader.ReadLine();
+                }
+                reader.Close();
+                response.Close();
+                return strs;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("获取文件出错：" + ex.Message);
+            }
+            return strs;
+        }
+
+        /// <summary>
+        /// 从ftp服务器上获得文件夹列表
+        /// </summary>
+        /// <param name="RequedstPath">服务器下的相对路径</param>
+        /// <returns></returns>
+        public static bool GetDirctory(string RequedstPath, string version)
+        {
+            try
+            {
+                string uri = RequedstPath;   //目标路径 path为服务器地址
+                FtpWebRequest reqFTP = (FtpWebRequest)FtpWebRequest.Create(new Uri(uri));
+                // ftp用户名和密码
+                reqFTP.Credentials = new NetworkCredential();
+                reqFTP.Method = WebRequestMethods.Ftp.ListDirectoryDetails;
+                WebResponse response = reqFTP.GetResponse();
+                StreamReader reader = new StreamReader(response.GetResponseStream());//中文文件名
+
+                string line = reader.ReadLine();
+                while (line != null)
+                {
+                    if (line.Contains(version))
+                    {
+                        return true;
+                    }
+                    line = reader.ReadLine();
+                }
+                reader.Close();
+                response.Close();
+                return false;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("获取目录出错：" + ex.Message);
+            }
+            return false;
+        }
+    }
+}
